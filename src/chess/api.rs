d294fa316473d9b23526b6b42438c5a047d19d87
Mounir Nessim <mmnessim@gmail.com>
2026-08-15 19:05:12 -0400

    add randomness

--- src/chess/api.rs
+++ src/chess/api.rs
@@ -11,15 +11,16 @@ pub async fn random_game(username: &str, client: reqwest::Client) {
     let url = format!("https://api.chess.com/pub/player/{username}/games/archives");
     if let Ok(response) = client.get(url).send().await {
         let text = response.text().await.unwrap();
-        let archives: Archives = serde_json::from_str(&text).unwrap();
-        if let Some(last) = archives.archives.last() {
-            if let Ok(res) = client.get(last).send().await {
-                let inner_text = res.text().await.unwrap();
-                let games: Vec<Game> = serde_json::from_str::<ManyGames>(&inner_text)
+        let archives = serde_json::from_str::<Archives>(&text).unwrap().archives;
+        let num = rand::random_range(0..archives.len());
+        let arch_choice = &archives[num];
+        if let Ok(res) = client.get(arch_choice).send().await {
+            let inner_text = res.text().await.unwrap();
+            let games: Vec<Game> = serde_json::from_str::<ManyGames>(&inner_text)
                     .unwrap()
                     .games;
-                println!("{:#?}", games.last());
-            }
+            let inner_num = rand::random_range(0..games.len());
+            println!("{:#?}", &games[inner_num]);
         }
     }
 }
